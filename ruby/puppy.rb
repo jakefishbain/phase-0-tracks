c7efe5a23c5b87ac42eb5bed55ca0c8ab40d3d82
Jake Fishbain <jakefishbain@gmail.com>
2016-07-25 13:35:43 -0500

    create 'blueprint' of puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+=begin
+ANIMAL ------------
+Puppy/Dog
+
+CHARACTERISTICS ---
+Color: varies
+Leg Count: 4
+Name: varies
+Tail Length: varies
+Friendly: yes
+
+BEHAVIOR ----------
+Bark
+Run
+Fetch
+	
+=end
