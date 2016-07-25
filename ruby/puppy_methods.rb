d4e60933c5da902f57baf87a18c6e26428b30905
Jake Fishbain <jakefishbain@gmail.com>
2016-07-25 15:39:28 -0500

    create superhero class and instantiate as well as explore puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,66 @@
-class Puppy
+# class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+#   def initialize
+#     p "Initializing new puppy instance..."
+#   end
+  
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
+
+#   def speak(n)
+#     n.times { puts "Woof!" }
+#   end
+  
+#   def roll_over
+#     p "*rolls over*"
+#   end
+  
+#   def dog_years(n)
+#     dog_years = n*7
+#     p dog_years
+#     dog_years
+#   end
+  
+#   def bite
+#     p "That hurt!"
+#   end
+  
+# end
+
+# buddy = Puppy.new
+# buddy.fetch("ball")
+# buddy.speak(10)
+# buddy.roll_over
+# buddy.dog_years(2)
+# buddy.bite
+
+
+
+class Superhero
+  
+  def initialize
+    p "Initializing a new Superhero instance"
   end
-
+  
+  def fly(place)
+    p "I'm flying to #{place}!"
+  end
+  
+  def punch(n)
+    n.times { puts "POW!" }
+  end
 end
 
+superman = Superhero.new
+superman.fly("home")
+superman.punch(3)
+
+heroes = []
+50.times { heroes << Superhero.new }
+
+heroes.each do |hero|
+  hero.fly("home")
+  hero.punch(3)
+end
