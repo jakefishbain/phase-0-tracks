42f55eb1a007f898108597fccfe645b0ce197b17
Jake Fishbain <jakefishbain@gmail.com>
2016-08-05 08:07:11 -0500

    create initial array and test looping strategies

--- js/algos.js
+++ js/algos.js
@@ -1,2 +1,11 @@
-//text
-//here we go!
+//allow driver code to input an array into a function being called
+//iterate original array into new data structure with key/value pairs for "phrase" and "length"
+//sort new array and/or pull out key/value pair with largest length and print to console
+
+function longest(arr) {
+  for (var i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+  }};
+
+longest(['eee', 'eeee', 'eeeee']);
+
