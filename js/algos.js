c0820deed800b157234109a5143c93a3bfb46f95
Jake Fishbain <jakefishbain@gmail.com>
2016-08-05 10:15:27 -0500

    create workflow for release 1

--- js/algos.js
+++ js/algos.js
@@ -30,3 +30,49 @@ function LongSort(original) {
 var sortGame = new LongSort(['dog','lamp','toast','cat', 'otomotopia']);
 var countedList = sortGame.phraseLength();
 console.log(sortGame.longest(countedList))
+
+
+//RELEASE 1
+
+var profile1 = {};
+function pair1(name, age){
+  profile1.name = name;
+  profile1.age = age;
+  return profile1
+};
+
+var profile2 = {};
+function pair2(name, age){
+  profile2.name = name;
+  profile2.age = age;
+  return profile2
+};
+
+
+console.log(pair1('Jake', 25));
+console.log(pair2('Brad', 22))
+//console.log(profile1.name)
+
+
+if (profile1.name == profile2.name) {
+  console.log('true');
+  return true;
+} else if (profile1.age == profile2.age) {
+  console.log('true');
+  return true;
+} else {
+  console.log('false');
+  return false;
+};
+
+
+
+
+
+
+
+
+
+
+
+
