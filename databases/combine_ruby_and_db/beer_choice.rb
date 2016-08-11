337dc1e79ca1be0257ca02075e3a374ed314b988
Jake Fishbain <jakefishbain@gmail.com>
2016-08-11 12:44:28 -0500

    create beer list hash and more UI

--- databases/combine_ruby_and_db/beer_choice.rb
+++ databases/combine_ruby_and_db/beer_choice.rb
@@ -5,7 +5,8 @@ db = SQLite3::Database.new("beer_choice.db")
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    favorite VARCHAR(255)
   )
 SQL
 
@@ -33,23 +34,59 @@ db.execute(create_table_cmd)
 #db.execute("INSERT INTO users (name) VALUES ('Jake')")
 
 
+initial_beers = {
+  "Gumball Head" => ["Pale Ale", 5.6],
+  "312" => ["Pale Ale", 4.2],
+  "Delirium Tremens" => ["Pale Ale", 8.5],
+  "Stella Artois" => ["Lager", 5.0],
+  "Guinness" => ["Stout", 4.2],
+  "Fresh Squeezed" => ["IPA", 6.4],
+  "Miller Lite" => ["Lager", 4.17],
+  "Dragonfly" => ["IPA", 6.0]
+}
+
+#puts initial_beers
+#puts initial_beers.keys
+#puts initial_beers["Gumball Head"][0] ======> Pale Ale
+
+puts db.execute("SELECT * from beers")
+
 def create_user(db, name)
   db.execute("INSERT INTO users (name) VALUES (?)", [name])
 end
 
+
+def save_favorite(db, name, favorite)
+end
+
+
+
+
+
+
+#-------USER INTERFACE-------------
 
 puts "Please type your name."
 user_name = gets.chomp.capitalize
 #p db.execute("SELECT name from users")
 
 if db.execute("SELECT name from users").join(' ').include?(user_name)
-  puts "You're already in our system, Let's get started!"
+  puts "You're already in our system, Let's get started!\n"
 else
   puts "Hmm, I don't see you, lets add to our system."
   create_user(db, user_name)
   if db.execute("SELECT name from users").join(' ').include?(user_name)
     puts "YAY, it worked!"
-  else
-    puts "Hmm, something went wrong..."
   end
 end
+
+puts "\nWould you like to learn about a particular beer? (y/n)"
+answer= gets.chomp.downcase
+
+if answer == 'y'
+  puts "\n"
+  puts "\nGreat! Here's a list of choices."
+  puts "\n"
+  puts initial_beers.keys
+end
+
