86b95c77cd37e381e79281d7f31bc7c54b19b4d6
Jake Fishbain <jakefishbain@gmail.com>
2016-08-12 09:00:35 -0500

    tested inserting values in both tables

--- databases/combine_ruby_and_db/feedback.rb
+++ databases/combine_ruby_and_db/feedback.rb
@@ -1,6 +1,6 @@
 require 'sqlite3'
 
-db = SQLite3::Database.new("feedback.db")
+db = SQLite3::Database.new('feedback.db')
 
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS users (
@@ -9,6 +9,7 @@ create_table_cmd = <<-SQL
     last_name VARCHAR(255)
   )
 SQL
+db.execute(create_table_cmd)
 
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS reviews (
@@ -16,6 +17,18 @@ create_table_cmd = <<-SQL
     day VARCHAR(255),
     comment VARCHAR(255),
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES users.id
+    FOREIGN KEY (user_id) REFERENCES users(id)
   )
 SQL
+db.execute(create_table_cmd)
+
+
+#add a test user
+# db.execute("INSERT INTO users (first_name, last_name) VALUES ('Jake', 'Fishbain')")
+users = db.execute("SELECT * FROM users")
+puts users
+#add a test review
+#db.execute("INSERT INTO reviews(day, comment, user_id) VALUES ('8/12/2016', 'Jake helped me with my coding!', 1)")
+reviews = db.execute("SELECT * FROM reviews")
+puts reviews
+
