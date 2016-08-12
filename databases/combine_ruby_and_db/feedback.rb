de45d806cf1dcff02fb927ee9d5247bf338ef65d
Jake Fishbain <jakefishbain@gmail.com>
2016-08-12 09:04:08 -0500

    create method to add users

--- databases/combine_ruby_and_db/feedback.rb
+++ databases/combine_ruby_and_db/feedback.rb
@@ -25,10 +25,18 @@ db.execute(create_table_cmd)
 
 #add a test user
 # db.execute("INSERT INTO users (first_name, last_name) VALUES ('Jake', 'Fishbain')")
-users = db.execute("SELECT * FROM users")
-puts users
+#users = db.execute("SELECT * FROM users")
+#puts users
+
 #add a test review
 #db.execute("INSERT INTO reviews(day, comment, user_id) VALUES ('8/12/2016', 'Jake helped me with my coding!', 1)")
-reviews = db.execute("SELECT * FROM reviews")
-puts reviews
+#reviews = db.execute("SELECT * FROM reviews")
+#puts reviews
 
+def create_user(db, first_name, last_name)
+  db.execute("INSERT INTO users (first_name, last_name) VALUES (?, ?)", [first_name, last_name])
+end
+
+create_user(db, "Brad", "Greenspan")
+users = db.execute("SELECT * FROM users")
+puts users
