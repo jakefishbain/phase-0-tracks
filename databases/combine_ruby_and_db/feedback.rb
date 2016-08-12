78eac5532c9d3c4ceb4c46ab977a465a878988fa
Jake Fishbain <jakefishbain@gmail.com>
2016-08-12 10:36:31 -0500

    create until loop for options and expand reviews table

--- databases/combine_ruby_and_db/feedback.rb
+++ databases/combine_ruby_and_db/feedback.rb
@@ -16,12 +16,13 @@ create_table_cmd = <<-SQL
     id INTEGERY PRIMARY KEY,
     day VARCHAR(255),
     comment VARCHAR(255),
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    reviewed_user_id INT,
+    reviewing_user_id INT,
+    FOREIGN KEY (reviewed_user_id) REFERENCES users(id),
+    FOREIGN KEY (reviewing_user_id) REFERENCES users(id)
   )
 SQL
 db.execute(create_table_cmd)
-
 
 #add a test user
 # db.execute("INSERT INTO users (first_name, last_name) VALUES ('Jake', 'Fishbain')")
@@ -40,13 +41,78 @@ end
 # test create_user method
 # create_user(db, "Brad", "Greenspan")
 users = db.execute("SELECT * FROM users")
-puts users
+#puts users
 
-def create_review(db, day, comment, user_id)
-  db.execute("INSERT INTO reviews (day, comment, user_id) VALUES (?, ?, ?)", [day, comment, user_id])
+def create_review(db, day, comment, reviewed_user_id, reviewing_user_id)
+  db.execute("INSERT INTO reviews (day, comment, reviewed_user_id, reviewing_user_id) VALUES (?, ?, ?, ?)", [day, comment, reviewed_user_id, reviewing_user_id])
 end
 
 # test create_review method
-create_review(db, '8/12/2016', 'Brad was awesome!', 2)
+# create_review(db, '8/12/2016', 'Brad was awesome!', 2, 1)
+# create_review(db, '8/11/2016', "Jake was great!", 1, 2)
 reviews = db.execute("SELECT * FROM reviews")
-puts reviews
+#puts reviews
+
+#---------------USER INTERFACE-------------------
+users.each do |id, first, last|
+  puts "#{id} | #{first} #{last}"
+end
+
+puts "\nHello and welcome to reviews on demand! Are you currently a user? (y/n)"
+current_user = gets.chomp.downcase
+
+#Determine if user, get id and/or create id
+if current_user == 'n' 
+  puts "Well, nice to meet you! What is your first name?"
+  first_name = gets.chomp.capitalize
+  puts "\nWhat is your last name?"
+  last_name = gets.chomp.capitalize
+  puts "\nGreat, I'm adding you to our system!"
+  create_user(db, first_name, last_name)
+elsif current_user == 'y'
+  puts "\nGreat! Let's get started!"
+else
+  puts "Let's try that again."
+end
+
+users.each do |id, first, last|
+  puts "#{id} | #{first} #{last}"
+end
+
+puts "\nWhat is your user id?"
+user_id = gets.chomp.to_i
+
+option = 5
+until option == 4 do
+  puts "\nPlease select from the following options:"
+  puts "1 | see reviews of me"
+  puts "2 | see reviews I have completed"
+  puts "3 | create a new review"
+  puts "4 | exit"
+  option = gets.chomp.to_i
+
+  if option == 1
+    info = db.execute("SELECT reviews.day, reviews.comment, reviewing_user_id FROM reviews WHERE reviews.reviewed_user_id = #{user_id}")
+    puts "\n"
+    puts info
+  elsif option == 2
+
+  elsif option == 3
+
+  end
+end
+
+#Would you like to create a review?
+
+
+
+
+
+
+
+
+
+
+
+
+
