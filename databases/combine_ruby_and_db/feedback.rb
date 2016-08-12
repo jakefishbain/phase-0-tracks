7debfe09104be962d03524ae039edb233cb63c4a
Jake Fishbain <jakefishbain@gmail.com>
2016-08-12 08:49:38 -0500

    create users and reviews table in feedback.db

--- databases/combine_ruby_and_db/feedback.rb
+++ databases/combine_ruby_and_db/feedback.rb
@@ -1,3 +1,21 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new("feedback.db")
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255)
+  )
+SQL
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS reviews (
+    id INTEGERY PRIMARY KEY,
+    day VARCHAR(255),
+    comment VARCHAR(255),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users.id
+  )
+SQL
