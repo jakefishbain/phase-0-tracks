f5b60e83bd956a0fe1f51974a6b0a3fa38c2b96d
Jake Fishbain <jakefishbain@gmail.com>
2016-08-18 09:11:38 -0500

    add contact page/parameter and great_job

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,33 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# A /contact route that displays an address (you can make up the address).
+
+get '/contact' do
+  "351 W. Hubbard, Chicago, IL"
+end
+
+
+# A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+
+# Query Parameters: localhost:9393?name=Jen
+# Route Parameters:
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+
+
+
+
+
+
+
+
