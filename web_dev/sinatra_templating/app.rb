94b1f603582350906a47ed7bb7a0c04b4b9b0392
Jake Fishbain <jakefishbain@gmail.com>
2016-08-18 12:04:45 -0500

    templating ruby

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,6 +17,11 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/search_students' do
+  erb :search_student
+end
+
+
 # create new students via
 # a form
 post '/students' do
@@ -24,4 +29,17 @@ post '/students' do
   redirect '/'
 end
 
+
+post '/find_students' do
+  age = params['age'].to_i
+  redirect "/search_results/#{age}"
+end
+
+get '/search_results/:age' do 
+  age = params[:age].to_i
+  @students = db.execute("SELECT * FROM students WHERE age=?", [age])
+  erb :search_results
+end
+
 # add static resources
+
